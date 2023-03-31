8cbd9a5a33c16d94bee9dfc7d13d71f8df0b4fd3
doylelew <69176020+doylelew@users.noreply.github.com>
2023-03-31 14:13:25 -0400

    Package Init

---
+++ SingKinter/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#!interpreter [3.11.2]
+
+__author__ = "Doyle Lewis"
+__copyright__ = "Copyright 2023, Doyle Lewis"
+__license__ = "Apache 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Doyle Lewis"
+__email__ = "odoylerules@pennyworth.network"
+__status__ = "Development"
+
+
